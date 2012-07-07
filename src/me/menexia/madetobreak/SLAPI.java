38382a0f68b3c77b7b1d07a726bb5a326d64beaf
Xavi Ablaza <xlablaza@gmail.com>
2012-07-07 22:58:13 +0800

    now licensed under CC BY-NC-SA 3.0

--- src/me/menexia/madetobreak/SLAPI.java
+++ src/me/menexia/madetobreak/SLAPI.java
@@ -1,3 +1,6 @@
+// This work is licensed under a Creative Commons Attribution-NonCommercial-ShareAlike 3.0 Unported License.	
+// To view a copy of this license, visit http://creativecommons.org/licenses/by-nc-sa/3.0/	
+// or send a letter to Creative Commons, 444 Castro Street, Suite 900, Mountain View, California, 94041, USA.
 package me.menexia.madetobreak;
 
 import java.io.FileInputStream;
